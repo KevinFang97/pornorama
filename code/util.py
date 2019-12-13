0df0bb0e30b3e7a9b17cd748d72eddc49f3ad208
Han Deng <han.deng@outlook.com>
2019-12-13 08:41:43 +0800

    update code and add comment

--- code/util.py
+++ code/util.py
@@ -40,7 +40,14 @@ def extract_ORB(image_list:List[np.ndarray]):
     return kp_list, feature_list
 
 '''
+
 return sets of feature matchees if there are multiple panoramas
+input: list of feature descriptors
+
+output: 1. set of sub maximum spanning tree with eahc node in each set like
+            (img_id1, img_id2, num_feature_matches)
+
+        2. dictionary of matches between images, keys are image ids.
 '''
 def feature_match(feature_list, threshold=50, SIFT=True):
     match_dic = {}
@@ -138,93 +145,82 @@ def maximum_spanning_tree(graph):
     return result
 
 
+if __name__ == '__main__':
 
-img_path1 = '/home/han/gitlab/CMU16720/HW2/HW2/data/adobe_panoramas/data/fishbowl/fishbowl-00.png'
-img_path2 = '/home/han/gitlab/CMU16720/HW2/HW2/data/adobe_panoramas/data/fishbowl/fishbowl-01.png'
+	img_path1 = '/home/han/gitlab/CMU16720/HW2/HW2/data/adobe_panoramas/data/fishbowl/fishbowl-00.png'
+	img_path2 = '/home/han/gitlab/CMU16720/HW2/HW2/data/adobe_panoramas/data/fishbowl/fishbowl-01.png'
+	img_path3 = '/home/han/gitlab/CMU16720/HW2/HW2/data/adobe_panoramas/data/fishbowl/fishbowl-02.png'
+	img_path4 = '/home/han/gitlab/CMU16720/HW2/HW2/data/adobe_panoramas/data/fishbowl/fishbowl-03.png'
+	img_path5 = '/home/han/gitlab/CMU16720/HW2/HW2/data/adobe_panoramas/data/halfdome/halfdome-00.png'
+	img_path6 = '/home/han/gitlab/CMU16720/HW2/HW2/data/adobe_panoramas/data/halfdome/halfdome-01.png'
+	img_path7 = '/home/han/gitlab/CMU16720/HW2/HW2/data/adobe_panoramas/data/halfdome/halfdome-02.png'
+	img_path8 = '/home/han/gitlab/CMU16720/HW2/HW2/data/adobe_panoramas/data/halfdome/halfdome-03.png'
 
-'''
-img_path3 = '/home/han/gitlab/CMU16720/HW2/HW2/data/adobe_panoramas/data/fishbowl/fishbowl-02.png'
-img_path4 = '/home/han/gitlab/CMU16720/HW2/HW2/data/adobe_panoramas/data/fishbowl/fishbowl-03.png'
+	img_color1 = cv2.imread(img_path1)
+	img_color2 = cv2.imread(img_path2)
+	img_color3 = cv2.imread(img_path3)
+	img_color4 = cv2.imread(img_path4)
+	img_color5 = cv2.imread(img_path5)
+	img_color6 = cv2.imread(img_path6)
+	img_color7 = cv2.imread(img_path7)
+	img_color8 = cv2.imread(img_path8)
 
-img_path5 = '/home/han/gitlab/CMU16720/HW2/HW2/data/adobe_panoramas/data/halfdome/halfdome-00.png'
-img_path6 = '/home/han/gitlab/CMU16720/HW2/HW2/data/adobe_panoramas/data/halfdome/halfdome-01.png'
-img_path7 = '/home/han/gitlab/CMU16720/HW2/HW2/data/adobe_panoramas/data/halfdome/halfdome-02.png'
-img_path8 = '/home/han/gitlab/CMU16720/HW2/HW2/data/adobe_panoramas/data/halfdome/halfdome-03.png'
-'''
-img_color1 = cv2.imread(img_path1)
-img_color2 = cv2.imread(img_path2)
-'''
-img_color3 = cv2.imread(img_path3)
-img_color4 = cv2.imread(img_path4)
-img_color5 = cv2.imread(img_path5)
-img_color6 = cv2.imread(img_path6)
-img_color7 = cv2.imread(img_path7)
-img_color8 = cv2.imread(img_path8)
-'''
+	#height, width,_ = img_color2.shape
+	#img_color2 = cv2.resize(img_color2, (int(width/2), int(height/2)))
+	#img_color2 = cv2.transpose(img_color2)
+	#img_color2 = cv2.flip(img_color2,flipCode=0)
 
-height, width,_ = img_color2.shape
-img_color2 = cv2.resize(img_color2, (int(width/2), int(height/2)))
-img_color2 = cv2.transpose(img_color2)
-img_color2 = cv2.flip(img_color2,flipCode=0)
+	# rotate cw
+	##out=cv2.transpose(img_color2)
+	#out=cv2.flip(out,flipCode=1)
 
-# rotate cw
-##out=cv2.transpose(img_color2)
-#out=cv2.flip(out,flipCode=1)
+	img_list = [img_color1, img_color2, img_color3, img_color4, img_color5, img_color6, img_color7, img_color8]
+	kp_list, feature_list = extract_ORB(img_list)
 
+    '''
+	kp1 = kp_list[0]
+	kp2 = kp_list[1]
 
-img_list = [img_color1, img_color2] #, img_color3, img_color4, img_color5, img_color6, img_color7, img_color8]
+	des1 = feature_list[0]
+	des2 = feature_list[1]
+	bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
+	matches = bf.match(des1, des2)
+	matches = sorted(matches, key=lambda x: x.distance)
 
+	img3 = cv2.drawMatches(img_color1,kp1, img_color2, kp2, matches[0:30], None,flags=cv2.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
+	plt.imshow(img3)
+	plt.show()
+    result, match_dic = feature_match(feature_list)
+    '''
 
-
-kp_list, feature_list = extract_ORB(img_list)
-
-
-kp1 = kp_list[0]
-kp2 = kp_list[1]
-
-des1 = feature_list[0]
-des2 = feature_list[1]
-bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
-matches = bf.match(des1, des2)
-matches = sorted(matches, key=lambda x: x.distance)
-
-img3 = cv2.drawMatches(img_color1,kp1, img_color2, kp2, matches[0:30], None,flags=cv2.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
-plt.imshow(img3)
-plt.show()
-#result, match_dic = feature_match(feature_list)
-
-'''
-#Visualizing the result
-'''
-
-'''
-for idx, each_result in enumerate(result):
-    img_set = set()
-    for pair in each_result:
-        img_set.add(pair[0])
-        img_set.add(pair[1])
-
-    img_id_list = list(img_set)
-
-    num_images = len(img_id_list)
-    height, width, channel = img_list[img_id_list[0]].shape
-    concatenated = np.zeros([height, width*4, channel])
-
-    for i in range(num_images):
-        concatenated[:, i*width:(i+1)*width,:] = img_list[img_id_list[i]]
+	'''
+	#Visualizing the result
+	'''
+	for idx, each_result in enumerate(result):
+	    img_set = set()
+	    for pair in each_result:
+		img_set.add(pair[0])
+		img_set.add(pair[1])
+	    img_id_list = list(img_set)
+	    num_images = len(img_id_list)
+	    height, width, channel = img_list[img_id_list[0]].shape
+	    concatenated = np.zeros([height, width*4, channel])
+	    for i in range(num_images):
+            concatenated[:, i*width:(i+1)*width,:] = img_list[img_id_list[i]]
 
 
-    for i in range(num_images-1):
-        idx1 = img_id_list[i]
-        idx2 = img_id_list[i+1]
-        matches = match_dic[(idx1, idx2)]
-        for each_match in matches[0:30]:
-            train_idx = each_match.trainIdx
-            query_idx = each_match.queryIdx
-            pos1 = kp_list[idx1][query_idx].pt
-            pos2 = kp_list[idx2][train_idx].pt
-            pos1 = (int(pos1[0]+i*width), int(pos1[1]))
-            pos2 = (int(pos2[0]+(i+1)*width), int(pos2[1]))
-            cv2.line(concatenated, pos1, pos2, (0, 255, 0), thickness=1, lineType=8)
-    cv2.imwrite('result{}.jpg'.format(idx+1), concatenated)
-'''
+	    for i in range(num_images-1):
+            idx1 = img_id_list[i]
+            idx2 = img_id_list[i+1]
+            matches = match_dic[(idx1, idx2)]
+
+		for each_match in matches[0:30]:
+		    train_idx = each_match.trainIdx
+		    query_idx = each_match.queryIdx
+		    pos1 = kp_list[idx1][query_idx].pt
+		    pos2 = kp_list[idx2][train_idx].pt
+		    pos1 = (int(pos1[0]+i*width), int(pos1[1]))
+		    pos2 = (int(pos2[0]+(i+1)*width), int(pos2[1]))
+		    cv2.line(concatenated, pos1, pos2, (0, 255, 0), thickness=1, lineType=8)
+
+	    cv2.imwrite('result{}.jpg'.format(idx+1), concatenated)
